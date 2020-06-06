d8a050ee2ce98bc2b06f765ab02f58e0fe623199
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2020-06-06 05:18:01 +0100

    Added Login form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 
-import styles from './Login.module.less';
-
+import Logo from '../../images/VAKT-logo.png'
+import styles from "./Login.module.less";
 
 const Login: React.FC = () => (
   <div className={styles.Login}>
-    Login Component
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src={Logo} style={styles.logo} /> Log-in to your account
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+
+            <Button color="teal" fluid size="large">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href="/#/signup">Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
   </div>
 );
 
