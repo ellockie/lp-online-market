2620f9592ec39fca310255c020b52ca5ef8cb2a2
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2020-06-08 08:17:54 +0100

    Adjusted tests - wrapped relevant compoenents in the store provider

--- src/components/Dashboard/Dashboard.test.tsx
+++ src/components/Dashboard/Dashboard.test.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { cleanup, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import '@testing-library/jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
+import { store } from '../../store/store';
 
 
 describe('<dashboard />', () => {
   afterEach(cleanup);
 
   test('it should mount', () => {
-    const { getByTestId } = render(<Dashboard />);
+    const { getByTestId } = render(<Provider store={store}><Dashboard /></Provider>);
     const dashboard = getByTestId('dashboard');
 
     expect(dashboard).toBeInTheDocument();
