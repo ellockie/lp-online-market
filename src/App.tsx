91322a75e6da595f488a0d725bbcd61de26fe2f4
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2020-06-11 10:59:43 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import {
   HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-  useHistory,
+  Switch
 } from "react-router-dom";
 
 import routes from "./routes/routes";
 import { TopBar } from "./components";
-
-import styles from "./App.module.css";
 
 const App: React.FC = (): JSX.Element => (
   <Router>
@@ -20,96 +13,5 @@ const App: React.FC = (): JSX.Element => (
     <Switch>{routes}</Switch>
   </Router>
 );
-
-function HomeButton() {
-  const history = useHistory();
-
-  function handleClick() {
-    history.push("/home");
-  }
-
-  return (
-    <button type="button" onClick={handleClick}>
-      Go home
-    </button>
-  );
-}
-
-function AboutButton() {
-  const history = useHistory();
-
-  function handleClick() {
-    history.push("/login");
-  }
-
-  return (
-    <button type="button" onClick={handleClick}>
-      Go to login
-    </button>
-  );
-}
-
-export function Home(): JSX.Element {
-  return (
-    <>
-      <h2>Home</h2>
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
-          <div>VAKT Global Market</div>
-        </header>
-      </div>
-    </>
-  );
-}
-
-
-export function About(): JSX.Element {
-  return (
-    <>
-      <h2>About</h2>
-    </>
-  );
-}
-
-export function Topics(): JSX.Element {
-  const match = useRouteMatch();
-
-  return (
-    <div className={styles.navBar}>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-          <HomeButton />
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-export function Topic(): JSX.Element {
-  const { topicId } = useParams();
-  return (
-    <>
-      <h3>Requested topic ID: {topicId}</h3>
-      <AboutButton />
-    </>
-  );
-}
 
 export default App;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import { About, Topics, Home } from "../App";
 import { Login } from "../components";
 import { Signup } from "../components";
 import { Dashboard } from "../components";
 import { ThankYou } from "../components";
 
 export default [
-    <Route key="Home" path="/home" component={Home} />,
     <Route key="Login" path="/login" component={Login} />,
     <Route key="Signup" path="/signup" component={Signup} />,
     <Route key="ThankYou" path="/thankyou" component={ThankYou} />,
     <Route key="Dashboard" path="/dashboard" component={Dashboard} />,
-    <Route key="About" path="/about" component={About} />,
-    <Route key="Topics" path="/topics" component={Topics} />,
 
     <Route key="Home-Login" path="/" component={Login} />,
 ];
