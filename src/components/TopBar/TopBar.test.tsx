58e6efbd5a8b5d95778307ef9f0b44a36b3fb8bb
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2020-06-16 01:44:26 +0100

    Fixed a test failing due to Router usage; cleanup

--- src/components/TopBar/TopBar.test.tsx
+++ src/components/TopBar/TopBar.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
 
 import TopBar from './TopBar';
 import { store } from '../../store/store';
@@ -10,7 +11,7 @@ describe('<TopBar />', () => {
   afterEach(cleanup);
 
   test('it should mount', () => {
-    const { getByTestId } = render(<Provider store={store}><TopBar /></Provider>);
+    const { getByTestId } = render(<Provider store={store}><Router><TopBar /></Router></Provider>);
     const topBar = getByTestId('TopBar');
 
     expect(topBar).toBeInTheDocument();
